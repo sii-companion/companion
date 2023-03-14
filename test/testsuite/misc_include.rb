5bf176893cf20b878576e83d661af045bbd6851f
Will Haese-Hill <wahhill@gmail.com>
2023-03-14 12:36:22 +0000

    deliberately fail test

--- test/testsuite/misc_include.rb
+++ test/testsuite/misc_include.rb
@@ -2,5 +2,5 @@ Name "gtscripts are runnable"
 Keywords "misc"
 Test do
   run_test("#{$bin}gt #{$bindir}/test.lua")
-  grep last_stdout, /travis test runs/
+  grep last_stdout, /travis test runsblah/
 end
